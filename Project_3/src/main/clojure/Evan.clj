564d51d3a0ec933a78f45764c5e2f59ef8275277
Luke-Knight54 <LukeVader54@outlook.com>
2025-05-01 14:01:12 -0400

    passes unit tests
    should work now

--- Project_3/src/main/clojure/Evan.clj
+++ Project_3/src/main/clojure/Evan.clj
@@ -13,25 +13,9 @@
     :else (cons (fun (first lizt)) 
                 (evanMap fun (rest lizt)))))  ;; Recur on the rest of the list
 
-(defn memq [element lizt] ;;made for the intersect def might not need later 
-	(lazy-seq
-		(cond
-			(nil? lizt)
-				false
-			(identical? (first lizt) element)
-				lizt
-			:else (memq element (rest lizt))
-		)
-	)
-)
-
 (defn intersect [lizt1 lizt2]
-	(cond 
-		(nil? lizt1)
-			'()
-		(memq (first lizt1) lizt2)
-			(cons (first lizt1) (intersect (rest lizt1) lizt2))
-		:else (intersect (rest lizt1) lizt2)
-	)
-)
-
+  (cond 
+    (empty? lizt1) '()
+    (member (first lizt1) lizt2)
+      (cons (first lizt1) (intersect (rest lizt1) lizt2))
+    :else (intersect (rest lizt1) lizt2)))
