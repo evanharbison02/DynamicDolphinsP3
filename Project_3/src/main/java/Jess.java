a32de644c7ec990d10bdd56923bd94440787c91e
Jessica Remphrey <151082431+JessicaRemphrey@users.noreply.github.com>
2025-05-01 01:04:43 -0400

    ???

---
+++ Project_3/src/main/java/Jess.java
@@ -0,0 +1,34 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jess {
+	public static boolean member (Object atm, List<Object> lizt) {
+		if (lizt == null || lizt.isEmpty()) {
+			return false;
+		}
+
+		if (atm.equals(lizt.get(0))) {
+			return true;
+		}
+
+		return member(atm, lizt.subList(1, lizt.size()));
+	}
+
+	public static List<Object> intersect (List<Object> lizt1, List<Object> lizt2) {
+		List<Object> listReturn = new ArrayList<Object>();
+
+		if (lizt1 == null || lizt1.isEmpty()) {
+			return listReturn;
+		}
+
+		Object first = lizt1.get(0);
+
+		if (lizt2.contains(first)) {
+			listReturn.add(first);
+		}
+		
+		listReturn.addAll(intersect(lizt1.subList(1, lizt1.size()), lizt2));
+		return listReturn;
+	}
+}
