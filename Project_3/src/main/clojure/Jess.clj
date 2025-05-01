73ba3898b66a5096dbcc6dc17aabbb42fdfd8ec3
Jessica Remphrey <151082431+JessicaRemphrey@users.noreply.github.com>
2025-05-01 01:10:03 -0400

    !!!

---
+++ Project_3/src/main/clojure/Jess.clj
@@ -0,0 +1,21 @@
+
+(defn append [lizt1 lizt2]
+  (cond
+    (empty? lizt1) lizt2
+    
+    :else (cons (first lizt1)
+      (append (rest lizt1) lizt2))
+  )
+)
+
+(defn same [lizt1 lizt2]
+  (cond
+    (empty? lizt1) (empty? lizt2)
+    (empty? lizt2) false
+
+    (= (first lizt1) (first lizt2))
+      (same (rest lizt1) (rest lizt2))
+
+    :else false
+  )
+)
